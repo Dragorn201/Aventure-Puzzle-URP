9cbbf5071e63fb831e025a3203cd4179f8b7fd02
Abal798 <158078797+Abal798@users.noreply.github.com>
2025-03-28 13:07:36 -0400

    camFix

--- Assets/scripts/game managing related scripts/CameraFollow.cs
+++ Assets/scripts/game managing related scripts/CameraFollow.cs
@@ -3,26 +3,34 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform player;         
+    public Transform player;
     public float positionSmoothSpeedWhenFollow = 0.125f;
     public float rotationSmoothSpeed = 3f;
     public Vector3 basicOffset;
     public Quaternion rotationOnPlayerFocus;
     public float camDelay;
     public float inputOffsetIntensity;
-    
-    [HideInInspector]public bool mustFollowPlayerPosition = true;
-    [HideInInspector]public bool MustBeBasicRotation = true;
+
+    [HideInInspector] public bool mustFollowPlayerPosition = true;
+    [HideInInspector] public bool MustBeBasicRotation = true;
     private Vector3 velocity = Vector3.zero;
-    [HideInInspector]public Vector3 desiredPosition;
-    [HideInInspector]public Quaternion desiredRotation;
+    [HideInInspector] public Vector3 desiredPosition;
+    [HideInInspector] public Quaternion desiredRotation;
     private float actualCamSpeed;
     private Vector3 actualCamOffset;
     private IEnumerator runningCoroutine;
     private bool coroutineIsRunning = false;
     private PlayerController playerController;
+    [HideInInspector] public bool aimAtPlayer = false;
     private Quaternion actualBaseRotation;
-    [HideInInspector]public bool aimAtPlayer = false;
+
+
+    private float angleStableTime = 0f;
+    public float requiredStableTime = 0.5f;
+    public float angleThreshold = 10f;
+    private Quaternion lastCheckedRotation;
+
+
 
     void Start()
     {
@@ -32,17 +40,18 @@ public class CameraFollow : MonoBehaviour
         desiredRotation = transform.rotation;
         actualBaseRotation = transform.rotation;
         actualCamSpeed = positionSmoothSpeedWhenFollow;
+        lastCheckedRotation = player.rotation;
     }
-    
+
     void LateUpdate()
     {
         desiredRotation = actualBaseRotation;
-        
+
         if (mustFollowPlayerPosition)
-        { 
+        {
             desiredPosition = player.position + actualCamOffset;
         }
-        
+
         if (MustBeBasicRotation)
         {
             desiredRotation = rotationOnPlayerFocus;
@@ -51,62 +60,73 @@ public class CameraFollow : MonoBehaviour
         if (aimAtPlayer)
         {
             desiredRotation = Quaternion.LookRotation(player.position - transform.position);
-            
         }
 
-        if (playerController.movementInput != Vector3.zero)
+        float angleDiffWithLast = Quaternion.Angle(lastCheckedRotation, player.rotation);
+        if (angleDiffWithLast < angleThreshold)
+        {
+            angleStableTime += Time.deltaTime;
+        }
+        else
+        {
+            angleStableTime = 0f;
+        }
+
+        if (angleStableTime >= requiredStableTime && playerController.movementInput != Vector3.zero)
         {
             Quaternion rotation = desiredRotation;
             Vector3 inputDirection = playerController.movementInput.normalized;
-            Quaternion inputOffsetRotation = Quaternion.Euler(inputOffsetIntensity * inputDirection.y, inputOffsetIntensity * inputDirection.x, 0); 
-            
-            desiredRotation = inputOffsetRotation * rotation; 
+            Quaternion inputOffsetRotation = Quaternion.Euler(inputOffsetIntensity * inputDirection.y,
+                inputOffsetIntensity * inputDirection.x, 0);
+
+            desiredRotation = inputOffsetRotation * rotation;
         }
 
+        lastCheckedRotation = player.rotation;
 
-        
-        
-        
-        
-        
         float angleDiff = Quaternion.Angle(transform.rotation, desiredRotation);
         if (angleDiff > 0.1f)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, Time.deltaTime * rotationSmoothSpeed);
+            transform.rotation =
+                Quaternion.Slerp(transform.rotation, desiredRotation, Time.deltaTime * rotationSmoothSpeed);
         }
-        else 
+        else
         {
             transform.rotation = desiredRotation;
         }
-        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, actualCamSpeed);
-       }
 
-    public void CallChangeCameraModeToStatic(bool isUnfixed ,Vector3 newCameraPosition , Quaternion newCameraRotation,  float newCamSpeed, bool lookPlayer)
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, actualCamSpeed);
+    }
+
+    public void CallChangeCameraModeToStatic(bool isUnfixed, Vector3 newCameraPosition, Quaternion newCameraRotation,
+        float newCamSpeed, bool lookPlayer)
     {
-        if(coroutineIsRunning)StopCoroutine(runningCoroutine);
+        if (coroutineIsRunning) StopCoroutine(runningCoroutine);
         runningCoroutine =
             ChangeCameraModeToStatic(isUnfixed, newCameraPosition, newCameraRotation, newCamSpeed, lookPlayer);
         StartCoroutine(runningCoroutine);
     }
 
-    public void CallChangeCameraModeToFollowPlayer(bool stop, Vector3 newOffset, Quaternion newCameraRotation, float newCamSpeed)
+    public void CallChangeCameraModeToFollowPlayer(bool stop, Vector3 newOffset, Quaternion newCameraRotation,
+        float newCamSpeed)
     {
-        if(coroutineIsRunning)StopCoroutine(runningCoroutine);
+        if (coroutineIsRunning) StopCoroutine(runningCoroutine);
         runningCoroutine = ChangeCameraModeToFollowPlayer(stop, newOffset, newCameraRotation, newCamSpeed);
         StartCoroutine(runningCoroutine);
     }
-    
-    
-    private IEnumerator ChangeCameraModeToStatic(bool isUnfixed ,Vector3 newCameraPosition , Quaternion newCameraRotation,  float newCamSpeed, bool lookPlayer)
+
+
+    private IEnumerator ChangeCameraModeToStatic(bool isUnfixed, Vector3 newCameraPosition,
+        Quaternion newCameraRotation, float newCamSpeed, bool lookPlayer)
     {
         coroutineIsRunning = true;
         yield return new WaitForSeconds(camDelay);
-        
+
         aimAtPlayer = false;
         mustFollowPlayerPosition = isUnfixed;
         MustBeBasicRotation = isUnfixed;
         actualCamSpeed = newCamSpeed;
-        
+
         if (!isUnfixed)
         {
             aimAtPlayer = lookPlayer;
@@ -114,14 +134,16 @@ public class CameraFollow : MonoBehaviour
             actualBaseRotation = newCameraRotation;
             desiredPosition = newCameraPosition;
         }
+
         coroutineIsRunning = false;
     }
 
-    private IEnumerator ChangeCameraModeToFollowPlayer(bool stop, Vector3 newOffset, Quaternion newCameraRotation, float newCamSpeed)
+    private IEnumerator ChangeCameraModeToFollowPlayer(bool stop, Vector3 newOffset, Quaternion newCameraRotation,
+        float newCamSpeed)
     {
         coroutineIsRunning = true;
         yield return new WaitForSeconds(camDelay);
-        
+
         MustBeBasicRotation = stop;
 
         if (!stop)
@@ -136,6 +158,7 @@ public class CameraFollow : MonoBehaviour
             actualCamOffset = basicOffset;
             actualBaseRotation = rotationOnPlayerFocus;
         }
+
         coroutineIsRunning = false;
     }
 }

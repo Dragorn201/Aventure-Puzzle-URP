2526ccb4402ac67cb2296e7fd1b24cf9bd10b456
moux25 <margaux.cad25@gmail.com>
2025-04-02 14:44:49 -0400

    declenche pas

--- Assets/Marg/Tool/DestructionSTP.cs
+++ Assets/Marg/Tool/DestructionSTP.cs
@@ -3,20 +3,20 @@ using HoudiniEngineUnity;
 
 public class DestructionSTP : MonoBehaviour
 {
-    public HEU_HoudiniAsset myHoudiniAsset; //jassigne mon asset dans l'inspector
+    public HEU_HoudiniAsset myHoudiniAsset; // HDA dans Unity
+    public string houdiniParameter = "StartDestruction"; // Nom du paramètre Houdini
 
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Collision détectée avec : " + other.gameObject.name); // Vérifie si le trigger fonctionne
-
-        if (other.CompareTag("Player")) // Vérifie si l'objet a bien le tag "Player"
+        if (other.CompareTag("Projectile")) // Si le personnage touche l'objet
         {
             Debug.Log("Le Player a touché l'objet !");
+
             if (myHoudiniAsset != null)
             {
-                Debug.Log("Houdini Asset trouvé, déclenchement de la destruction !");
-                myHoudiniAsset.RequestCook(true, true); // Force le recalcul de la simulation
+                myHoudiniAsset.SetBoolParameter(houdiniParameter, 1); // Active la destruction
+                myHoudiniAsset.RequestCook(true, true); // Force le recook de l'HDA
             }
             else
             {

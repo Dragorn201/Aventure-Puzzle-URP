40ea03d9a667587b2c594f8900daa7373eed7640
moux25 <margaux.cad25@gmail.com>
2025-04-04 16:33:47 -0400

    s

--- Assets/Marg/script/AmbienceSound.cs
+++ Assets/Marg/script/AmbienceSound.cs
@@ -5,7 +5,7 @@ public class AmbienceSound : MonoBehaviour
 
     public Collider Area;
     public GameObject Player;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         

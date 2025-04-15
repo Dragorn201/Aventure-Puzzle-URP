3267db1a160de19ca85220f76480f507c060783c
Dragorn201 <nathan.vasseur@orange.fr>
2025-04-15 11:07:03 -0400

    Reapply "Merge branch 'main' into Nathan"
    This reverts commit 19d13d468853f36416747893af0beb87ff9704e8.

---
+++ Assets/Marg/Destruction/Fracture.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+
+public class Fracture : MonoBehaviour
+{
+    public GameObject fractured;
+    public string triggerTag = "Projectile";
+
+    public float explosionForce = 500f;
+    public float explosionRadius = 2f;
+
+    private bool hasFractured = false;  
+
+    private void Start()
+    {
+        if (fractured != null)
+        {
+            fractured.SetActive(false);
+        }
+
+    }
+
+
+
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (hasFractured) return;
+
+
+        if (collision.gameObject.CompareTag(triggerTag))
+        {
+            hasFractured = true;
+
+            GameObject fractureObj = Instantiate(fractured, transform.position, transform.rotation);
+
+            foreach (Rigidbody rb in fractureObj.GetComponentsInChildren<Rigidbody>())
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+            }
+
+
+            fractured.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}

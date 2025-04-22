69404f1d6abfe24e280ce1d8b86944f49f7d67ce
Abal798 <158078797+Abal798@users.noreply.github.com>
2025-04-22 12:56:55 -0400

    placeholder sfx

--- Assets/scripts/environment behaviour/WallDestroy.cs
+++ Assets/scripts/environment behaviour/WallDestroy.cs
@@ -6,12 +6,19 @@ public class WallDestroy : MonoBehaviour
 {
     [SerializeField] float minSpeedToDestroyWall = 0.1f;
     public UnityEvent onWallDestroyed;
+    private SoundManager soundManager;
+
+    void Awake()
+    {
+        soundManager = FindAnyObjectByType<SoundManager>();
+    }
 
     public bool TryDestroyWall(float speed)
     {
         if (speed > minSpeedToDestroyWall)
         {
             onWallDestroyed.Invoke();
+            soundManager.PlaySoundEffect(soundManager.playerDestroyWall);
             return true;
         }
         return false;

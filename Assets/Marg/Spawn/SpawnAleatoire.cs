7f082c38173e78c62f31e44818a3d59bed17ba5e
moux25 <margaux.cad25@gmail.com>
2025-04-23 13:32:24 -0400

    modif spawn aleatoire

--- Assets/Marg/Spawn/SpawnAleatoire.cs
+++ Assets/Marg/Spawn/SpawnAleatoire.cs
@@ -4,41 +4,55 @@ using System.Collections.Generic;
 
 public class SpawnAleatoire : MonoBehaviour
 {
-
-    [Header("Spawn Settings")]
+    [Header("Spawn settings")]
     public GameObject resourcePrefab;
     public float spawnChance;
 
-    [Header("Raycast Settings")]
-    public float distanceBetweenChecks;
+    [Header("Raycast setup")]
+    public float distanceBetweenCheck;
     public float heightOfCheck = 10f, rangeOfCheck = 30f;
     public LayerMask layerMask;
     public Vector2 positivePosition, negativePosition;
 
-
-
-
     private void Start()
     {
-        SpawnRessources();
+        SpawnResources();
     }
 
-
-    void SpawnRessources()
+    private void Update()
     {
-        for (float x = negativePosition.x; x < positivePosition.x; x += distanceBetweenChecks)
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            for (float z = negativePosition.y; z < positivePosition.y; z += distanceBetweenChecks)
+            DeleteResources();
+            SpawnResources();
+        }
+    }
+
+    void SpawnResources()
+    {
+        for (float x = negativePosition.x; x < positivePosition.x; x += distanceBetweenCheck)
+        {
+            for (float z = negativePosition.y; z < positivePosition.y; z += distanceBetweenCheck)
             {
                 RaycastHit hit;
                 if (Physics.Raycast(new Vector3(x, heightOfCheck, z), Vector3.down, out hit, rangeOfCheck, layerMask))
                 {
-
+                    if (spawnChance > Random.Range(0f, 101f))
+                    {
+                        Instantiate(resourcePrefab, hit.point, Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0)), transform);
+                    }
                 }
             }
         }
     }
 
+    void DeleteResources()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
 
 
 }

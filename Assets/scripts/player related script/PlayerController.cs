02800efdb53fc240cc813d201b62c0a8be32bf53
Abal798 <158078797+Abal798@users.noreply.github.com>
2025-04-28 19:06:38 -0400

    update + fixs

--- Assets/scripts/player related script/PlayerController.cs
+++ Assets/scripts/player related script/PlayerController.cs
@@ -201,7 +201,6 @@ public class PlayerController : MonoBehaviour
             if (Physics.Raycast(transform.position + offset, dirOnStart, Vector3.Distance(transform.position, Vector3.MoveTowards(transform.position, targetPoint, actualSpeed))))
             {
                 moveSpeed = basicSpeed;
-                Debug.Log(offset);
                 break;
             }
             
@@ -227,7 +226,7 @@ public class PlayerController : MonoBehaviour
             yield return new WaitForFixedUpdate();
         }
         
-        transform.position = previousPos - offset;
+        transform.position = previousPos;
         
         GettingOnWall(interrupted ,basicSpeed ,hit ,dirOnStart);
     }

fd3a9310f8f398ec1644acf06b5ed0364b4bf2d3
sopastic <166405522+sopastic@users.noreply.github.com>
2025-05-07 23:17:03 -0400

    Implementation shader brulure quand cloche sonne

---
+++ Assets/Scenes/SopaTests/DissolvingRonces.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class DissolvingRonces : MonoBehaviour
+{
+    public Material material;
+
+    public float dissolveRate = 0.0125f;
+    public float refreshRate = 0.025f;
+    public string parameter = "";
+
+    void Awake()
+    {
+        material.SetFloat(parameter, 0);
+    }
+
+    IEnumerator DissolvingCo()
+    {
+        float counter = 0;
+
+        while (material.GetFloat(parameter) < 100)
+        {
+            counter += dissolveRate;
+            material.SetFloat(parameter, counter);
+
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
+
+    public void BeginDissolving()
+    {
+        StartCoroutine(DissolvingCo());
+    }
+}

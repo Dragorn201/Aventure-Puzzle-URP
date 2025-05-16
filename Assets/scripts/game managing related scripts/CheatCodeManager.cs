c28d946d9f939cf8743d891423a18caa63e37052
Abal798 <158078797+Abal798@users.noreply.github.com>
2025-05-16 19:46:40 -0400

    begin cheatcode manager

---
+++ Assets/scripts/game managing related scripts/CheatCodeManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CheatCodeManager : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject[] steps;
+    private int currentStep = 0;
+    
+    void Update()
+    {
+        
+    }
+
+    void GoNextStep()
+    {
+        
+    }
+
+    void GoPreviousStep()
+    {
+        
+    }
+}
